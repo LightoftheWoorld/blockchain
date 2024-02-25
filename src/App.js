44c9bee3425a5b681d9013e9717e68d5ced380cc
Victor Faith <vfaith37@gmail.com>
2024-02-25 15:54:43 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/shared/Layout";
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import OTP from "./pages/OTP"
-import Forgotpassword from "./pages/Forgotpassword"
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import OTP from "./pages/OTP";
+import Forgotpassword from "./pages/Forgotpassword";
 import Overview from "./components/Overview";
 import AuthLayout from "./components/shared/AuthLayout";
 import Election from "./components/Election";
 import Candidates from "./components/Candidates";
 import Create from "./components/Create";
+import Landing from "./pages/Landing";
 // import Layout from "./components/shared/layout";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="dashboard" element={<Layout />}>
-          <Route index element={<Overview />} ></Route>
-          <Route path="election" element={<Election />}></Route>
-          <Route path="candidate" element={<Candidates />} ></Route>
-          <Route path="create" element={<Create />} ></Route>
-        </Route>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />}></Route>
-          <Route path="signup" element={<Signup />} ></Route>
-          <Route path="forgotpassword" element={<Forgotpassword />} ></Route>
-          <Route path="otp" element={<OTP />} ></Route>
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />}></Route>
+          <Route path="dashboard" element={<Layout />}>
+            <Route index element={<Overview />}></Route>
+            <Route path="election" element={<Election />}></Route>
+            <Route path="candidate" element={<Candidates />}></Route>
+            <Route path="create" element={<Create />}></Route>
+          </Route>
+          <Route path="organization" element={<AuthLayout />}>
+            <Route index element={<Login />}></Route>
+            <Route path="signup" element={<Signup />}></Route>
+            <Route path="forgotpassword" element={<Forgotpassword />}></Route>
+            <Route path="otp" element={<OTP />}></Route>
+          </Route>
+        </Routes>
+      </Router>
+      {/* <Landing /> */}
+    </>
   );
 }
 
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+import WEB_IMAGE from "../assets/web.png";
+import LOGO from "../assets/logo.png";
+import { FaInstagram, FaLinkedin, FaXTwitter } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
+
+export default function Landing() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="w-full h-full bg-black text-white py-10 px-36 scroll-smooth touch-auto">
+      <div className="flex justify-between h-10 items-center">
+        <div className="flex items-center gap-2 px-2 py-3">
+          <img src={LOGO} alt="blockvote" className="h-4" />
+          <h1 className="text-xl font-bold mr-auto">blockvote</h1>
+        </div>
+
+        <div className="flex gap-20">
+          <a href="#services" class="text-white hover:text-teal-600">
+            <div className="text-lg font-medium">Services</div>
+          </a>
+          <a href="#about" class="text-white hover:text-teal-600">
+            <div className="text-lg font-medium">About</div>
+          </a>
+          <a href="#contact" class="text-white hover:text-teal-600">
+            <div className="text-lg font-medium">Contact</div>
+          </a>
+        </div>
+
+        <div className="flex gap-10">
+          <button
+            onClick={() => navigate("/organization")}
+            className="w-28 h-12 bg-teal-600 justify-center items-center inline-flex cursor-pointer"
+          >
+            Log In
+          </button>
+          <button
+            onClick={() => navigate("organization/signup")}
+            className="text-lg font-medium w-28 h-12 border justify-center items-center inline-flex cursor-pointer"
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-between py-20 items-center">
+        <div className="w-[587px]">
+          <p className="text-6xl font-bold pb-8">
+            <span className="text-teal-600">Secure</span> &{" "}
+            <span className="text-teal-600">accessible</span> voting at your
+            fingertips.
+          </p>
+          <p className="text-white text-base font-medium">
+            A specialized voting platform based on{" "}
+            <span className="text-neutral-600 font-extrabold">blockchain</span>{" "}
+            designed for Voters and Candidates, facilitating a smooth electoral
+            process across multiple <br />
+            channels.
+          </p>
+          <div className="bg-teal-600 w-36 h-14 flex justify-center items-center my-8">
+            <p className="text-white text-base font-bold">Get Started</p>
+          </div>
+        </div>
+        <img src={WEB_IMAGE} alt="web" className="w-[550px] h-[550px]" />
+      </div>
+
+      <div className="py-4" id="services">
+        <div className="flex flex-col items-center">
+          <p className="text-teal-600 text-2xl font-bold">WHAT WE DO</p>
+          <p className="text-4xl font-bold">Our mission explained</p>
+          <p className="text-2xl font-normal">
+            Our mission explained We leverage cutting-edge technology so people
+            can vote securely from anywhere and <br />
+            administrators can manage elections with ease.
+          </p>
+        </div>
+      </div>
+
+      <div className="py-6" id="about">
+        <p className="text-6xl font-medium text-center">
+          Everything you need to <br />
+          know
+        </p>
+        <div className="flex flex-col items-start py-20 gap-20">
+          <p className="text-3xl font-medium tracking-wide">
+            Do you possess the knowledge to help me with my election?
+          </p>
+          <p className="text-3xl font-medium tracking-wide">
+            What is Blockvote
+          </p>
+          <p className="text-3xl font-medium tracking-wide">
+            Are there any gas fees incurred
+          </p>
+          <p className="text-3xl font-medium tracking-wide">
+            How can I get a proposal?
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-between" id="contact">
+        <div>
+          <p className="text-4xl font-bold">
+            Say Hi to our <br />
+            team
+          </p>
+          <div className="flex gap-2 py-4">
+            {/* <HiOutlineInsta */}
+            <FaXTwitter className="h-10 w-11" />
+            <FaInstagram className="h-10 w-11" />
+            <FaLinkedin className="h-10 w-11" />
+          </div>
+        </div>
+        <div className="flex gap-8">
+          <div className="">
+            <p className="text-2xl font-medium">Contact</p>
+            <p className="text-zinc-400 text-xl font-normal">
+              +234-913-9073-466
+            </p>
+            <p className="text-zinc-400 text-xl font-normal">
+              Babcock University
+            </p>
+          </div>
+          <div className="">
+            <p className="text-2xl font-medium">Socials</p>
+            <p className="text-zinc-400 text-xl font-normal">Instagram</p>
+            <p className="text-zinc-400 text-xl font-normal">X</p>
+            <p className="text-zinc-400 text-xl font-normal">Linkedin</p>
+          </div>
+          <div className="">
+            <p className="text-2xl font-medium">Company</p>
+            <p className="text-zinc-400 text-xl font-normal">About</p>
+            <p className="text-zinc-400 text-xl font-normal">Terms</p>
+            <p className="text-zinc-400 text-xl font-normal">Privacy</p>
+          </div>
+          <div className="">
+            <p className="text-2xl font-medium">Products</p>
+            <p className="text-zinc-400 text-xl font-normal">Busa APP</p>
+            <p className="text-zinc-400 text-xl font-normal">Ease</p>
+          </div>
+        </div>
+      </div>
+      <p className="py-4 text-center opacity-70 text-zinc-400 text-xl font-normal">
+        The future of voting is just a tap away
+      </p>
+    </div>
+  );
+}
