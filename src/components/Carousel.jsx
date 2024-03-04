763a2d9bc772aa2e6c25525769a807ac51f314e1
Victor Faith <vfaith37@gmail.com>
2024-03-04 14:05:07 +0100

    update

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,48 +1,8 @@
 import React, { useState } from "react";
-import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+import { HiCheck, HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
-const Carousel = () => {
-  //   const [currentIndex, setCurrentIndex] = useState(1);
-  const data = [
-    {
-      image: "https://unsplash.it/640/425?image=30",
-      firstname: "stephen",
-      lastname: "dada",
-      course: "SE"
-    },
-    {
-      image: "https://unsplash.it/640/425?image=40",
-      firstname: "victor",
-      lastname: "fagbohun",
-      course: "CS"
-    },
-    {
-      image: "https://unsplash.it/640/425?image=50",
-      firstname: "taiwo",
-      lastname: "ariyo",
-      course: "CS"
-    },
-    {
-      image: "https://unsplash.it/640/425?image=10",
-      firstname: "praise",
-      lastname: "ibe",
-      course: "CS"
-    },
-    {
-      image: "https://unsplash.it/640/425?image=20",
-      firstname: "iyunade",
-      lastname: "victor",
-      course: "CS"
-    },
-    {
-      image: "https://unsplash.it/640/425?image=60",
-      firstname: "boye",
-      lastname: "odafe",
-      course: "CS"
-    },
-  ];
-
-
+const Carousel = (props) => {
+  const { data } = props;
   const previous = () => {
     if (currentIndex > 1) {
       setCurrentIndex(currentIndex - 1);
@@ -57,7 +17,6 @@ const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const selectImage = (index) => {
     setCurrentIndex(index);
-    console.log(index);
   };
 
   return (
@@ -69,16 +28,26 @@ const Carousel = () => {
       />
       {data.map((item, index) => (
         <div className="flex flex-col items-center">
-          <img
-            key={index}
-            src={item.image}
-            alt="candidate"
-            className={`rounded-full cursor-pointer w-32 h-32 ${
-              index === currentIndex ? "border-4 border-teal-600" : ""
-            }`}
-            onClick={() => selectImage(index)}
-          />
-          <p className="text-xl font-bold mt-[26px]">{item.firstname.charAt(0).toUpperCase() + item.firstname.slice(1)} {item.lastname.charAt(0).toUpperCase() + item.lastname.slice(1)}</p>
+          {currentIndex === index ? (
+            <div className="rounded-full cursor-pointer w-32 h-32 flex justify-center items-center bg-teal-600">
+              <HiCheck size={70} />
+            </div>
+          ) : (
+            <img
+              key={index}
+              src={item.image}
+              alt="candidate"
+              className={`rounded-full cursor-pointer w-32 h-32 ${
+                index === currentIndex ? "border-4 border-teal-600" : ""
+              }`}
+              onClick={() => selectImage(index)}
+            />
+          )}
+
+          <p className="text-xl font-bold mt-[26px]">
+            {item.firstname.charAt(0).toUpperCase() + item.firstname.slice(1)}{" "}
+            {item.lastname.charAt(0).toUpperCase() + item.lastname.slice(1)}
+          </p>
           <p className="mt-[7px] text-neutral-500 text-base">{item.course}</p>
         </div>
       ))}
--- src/components/VotersWelcome.jsx
+++ src/components/VotersWelcome.jsx
@@ -1,10 +1,5 @@
-import React, { useState } from "react";
-import {
-  HiArrowNarrowRight,
-  HiCheck,
-  HiChevronLeft,
-  HiChevronRight,
-} from "react-icons/hi";
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 import { useNavigate } from "react-router-dom";
 import Carousel from "./Carousel";
 import { Data } from "../data/dummydata";
@@ -48,63 +43,12 @@ export function ElectionInfo() {
 }
 
 export function Vote() {
-  const previous = () => {
-    if (currentIndex > 1) {
-      setCurrentIndex(currentIndex - 1);
-    }
-  };
-
-  const forward = () => {
-    if (currentIndex < Data.length) {
-      setCurrentIndex(currentIndex + 1);
-    }
-  };
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const selectImage = (index) => {
-    setCurrentIndex(index);
-  };
-
+    
   return (
     <div className="mt-4 flex flex-col items-center">
       <p className="text-white font-bold text-[77px]">President</p>
-      <div className="flex gap-20 justify-between my-[68px] items-center">
-        <HiChevronLeft
-          className="text-neutral-500 cursor-pointer"
-          onClick={previous}
-          size={120}
-        />
-        {Data.map((item, index) => (
-          <div className="flex flex-col items-center">
-            {index === currentIndex ? (
-              <div className="w-32 h-32 items-center flex justify-center bg-teal-600 rounded-full">
-                <HiCheck  size={70} />
-              </div>
-            ) : (
-                <img
-                    key={index}
-                    src={item.image}
-                    alt="candidate"
-                    className={`rounded-full cursor-pointer w-32 h-32 ${
-                    index === currentIndex ? "border-4 border-teal-600" : ""
-                    }`}
-                    onClick={() => selectImage(index)}
-                />
-            )}
-
-            <p className="text-xl font-bold mt-[26px]">
-              {item.firstname.charAt(0).toUpperCase() + item.firstname.slice(1)}{" "}
-              {item.lastname.charAt(0).toUpperCase() + item.lastname.slice(1)}
-            </p>
-            <p className="mt-[7px] text-neutral-500 text-base">{item.course}</p>
-          </div>
-        ))}
-        <HiChevronRight
-          className="text-neutral-500 cursor-pointer"
-          onClick={forward}
-          size={120}
-        />
-      </div>
-      <p className="text-neutral-500 text-[35px] font-normal">
+      <Carousel data={Data} />
+      <p className="text-neutral-500 text-[28px] font-normal">
         choose your preferred candidate
       </p>
       <button className="flex gap-2 items-center w-32 h-14 bg-teal-600 justify-center mt-[120px]">
